13297cddc3580a93405afdca9e1d82c371672e3d
Ry Sutton <ryancesutton@gmail.com>
2020-06-23 14:44:05 -0500

    create post table through post class and add jpa repository

---
+++ src/main/java/com/codeup/blog/models/Post.java
@@ -0,0 +1,58 @@
+package com.codeup.blog.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "posts")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length = 200)
+    private String title;
+
+    @Column(nullable = false)
+    private String body;
+
+    // spring framework uses this empty constructor
+    public Post(){}
+
+    //insert
+    public Post(String title, String body){
+        this.title = title;
+        this.body = body;
+    }
+
+    //read
+    public Post(long id, String title, String body) {
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
