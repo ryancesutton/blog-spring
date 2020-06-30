141a6fb03e260e881f0024a063da813334a6606b
Ry Sutton <ryancesutton@gmail.com>
2020-06-30 10:33:35 -0500

    add user class and define post-user relationship

--- src/main/java/com/codeup/blog/models/Post.java
+++ src/main/java/com/codeup/blog/models/Post.java
@@ -16,6 +16,9 @@ public class Post {
     @Column(nullable = false)
     private String body;
 
+    @OneToOne
+    private User owner;
+
     // spring framework uses this empty constructor
     public Post(){}
 
